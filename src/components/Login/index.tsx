7acc134f6cc4eb8fc8adbd1a0ecf69740d99491f
dk <hi@mrkha.ng>
2025-02-24 05:31:41 +0100

    updated UI for example demo purposes

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -9,6 +9,14 @@ export const Login: FunctionComponent<RoutableProps> = () => {
         <input type="password" name="password" required></input>
         <input type="submit" value="login" aria-label="login"></input>
       </form>
+      <br />
+      <div>(demo:demo)</div>
+      {
+        /* @TODO consider removing external css <link> once the issue
+        * https://github.com/fullsoak/fullsoak/issues/17 is fixed
+        */
+      }
+      <link href="/components/Login/styles.css" rel="stylesheet" />
     </section>
   );
 };
